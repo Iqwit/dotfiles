0cd5b9f6dc4933f58277372d16d7158bb08d1047
Felix Davies <f2lix@outlook.com>
2024-09-02 12:29:57 +0100

    Update install script

--- install.sh
+++ install.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+cd ~/dotfiles
+
+OLD_CONF="~/.config-old"
+CONF="~/.config"
+
+backup_config () {
+  if ! [ -d "$OLD_CONF" ]; then
+    mkdir "$OLD_CONF"
+  fi
+  subdir="$1"
+  if [ -d "$OLD_CONF/$subdir" ]; then
+    mv "$CONF/$subdir" "$OLD_CONF"
+  fi
+}
+
+backup_config hypr
+backup_config waybar
+
+cp .config/hypr "$CONF/hypr"
