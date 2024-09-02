248bd7df13e84454ada6f1718b7047a6a7761d0b
Felix Davies <f2lix@outlook.com>
2024-09-02 16:11:58 +0100

    Change logic of install script

--- install.sh
+++ install.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
-cd ~/dotfiles
+cd $HOME/dotfiles
 
 OLD_CONF="$HOME/.config-old"
 CONF="$HOME/.config"
 
-backup_config () {
-  if ! [ -d "$OLD_CONF" ]; then
-    mkdir "$OLD_CONF"
-  fi
-  subdir="$1"
-  if [ -d "$OLD_CONF/$subdir" ]; then
-    mv "$CONF/$subdir" "$OLD_CONF"
-  fi
-}
-
-backup_config hypr
-backup_config waybar
+cp "$CONF" "$OLD_CONF"
 
 cp -r .config/hypr "$CONF"
 cp -r .config/waybar "$CONF"
